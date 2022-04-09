bbbb15a8992cacd8ea2534e61e0e8c4b5a168e75
Mindvezar <99460016+mindmintt@users.noreply.github.com>
2022-04-09 13:42:56 +0000

    Initial commit

---
+++ src/manu.svelte
@@ -0,0 +1,55 @@
+<script>
+
+</script>
+
+<main>
+ 
+  <h1>Welcome!</h1>
+  <span class = 'btn'>
+    <button class = 'btn1'>ลงทะเบียนเรียน</button><br>
+    <button class = 'btn2'>ผลการลงทะเบียน</button><br>
+    <button class = 'btn3'>คู่มือการลงทะเบียน</button><br>
+    <button class = 'btn4'> ตารางเรียน</button><br>
+    <button class = 'btn5'>ถอนรายวิชา</button><br>
+  </span>
+
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+    background: linear-gradient(-45deg,#E4BAFF,#AEBAF8);
+  }
+
+  h1 {
+    color: white;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 15rem;
+  }
+  button{
+    padding: 20px 50px;
+    width: 17rem;
+    border : 0 px ;
+    border-radius: 15px;
+    border-color:white;
+    background : transparent;
+    margin: 5px;
+    
+    color: white;
+    text-align: center;
+    font-size: 1.10rem;
+  }
+
+  
+</style>
